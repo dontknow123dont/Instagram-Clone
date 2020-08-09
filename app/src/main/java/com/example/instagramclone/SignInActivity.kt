915fdde1257de090be58dcf3992b1774983a1a23
souvikcode <souvikcucse@gmail.com>
2020-08-09 21:00:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/instagramclone/SignInActivity.kt
@@ -0,0 +1,54 @@
+package com.example.instagramclone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity() {
+
+    var mAuth: FirebaseAuth? = null
+    var mAuthListener: FirebaseAuth.AuthStateListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        mAuth = FirebaseAuth.getInstance()
+        mAuthListener = FirebaseAuth.AuthStateListener {
+        }
+    }
+
+    fun signIn(view : View){
+        mAuth!!.signInWithEmailAndPassword(emailText.text.toString(),passwordText.text.toString())
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(applicationContext, FeedActivity::class.java)
+                    startActivity(intent)
+                }
+            }.addOnFailureListener { exception ->
+                if (exception != null) {
+                    Toast.makeText(applicationContext,exception.localizedMessage,Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    fun signUp(view: View){
+        mAuth!!.createUserWithEmailAndPassword(emailText.text.toString(),passwordText.text.toString())
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext,"User Created",Toast.LENGTH_LONG).show()
+                    val intent = Intent(applicationContext, FeedActivity::class.java)
+                    startActivity(intent)
+                }
+            }.addOnFailureListener { exception ->
+                if (exception != null) {
+                    Toast.makeText(applicationContext,exception.localizedMessage,Toast.LENGTH_LONG).show()
+                }
+            }
+
+    }
+}
